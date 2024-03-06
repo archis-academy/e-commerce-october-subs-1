f546fb517affa20720bf64ee53a7103b4942a948
emre yüce <115780348+eycwave@users.noreply.github.com>
2024-03-06 23:32:46 +0300

    update

--- src/main/java/com/archisacademy/ecommerceoctobersubs1/model/Refund.java
+++ src/main/java/com/archisacademy/ecommerceoctobersubs1/model/Refund.java
@@ -12,7 +12,7 @@ public class Refund {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Integer id;
+    private Long id;
     @Column(name = "refund_address")
     private String refundAddress;
     @Column(name = "reason")
