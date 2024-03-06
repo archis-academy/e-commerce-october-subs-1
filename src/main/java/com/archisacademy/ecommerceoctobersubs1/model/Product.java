3b71c7be373195f1e55ec1ae450d9a7467348c47
emre yüce <115780348+eycwave@users.noreply.github.com>
2024-03-06 23:33:57 +0300

    update

--- src/main/java/com/archisacademy/ecommerceoctobersubs1/model/Product.java
+++ src/main/java/com/archisacademy/ecommerceoctobersubs1/model/Product.java
@@ -10,7 +10,7 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
-    private Integer id;
+    private Long id;
     @Column(name="product_name")
     private String productName;
     @Column(name="description")
