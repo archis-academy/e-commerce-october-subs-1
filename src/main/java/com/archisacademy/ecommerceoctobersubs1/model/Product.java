7cb952c6583c529ea1a0946a68899a70df3375b2
eycwave <emreyuce000@gmail.com>
2024-03-06 17:32:38 +0300

    update

---
+++ src/main/java/com/archisacademy/ecommerceoctobersubs1/model/Product.java
@@ -0,0 +1,23 @@
+package com.archisacademy.ecommerceoctobersubs1.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "products")
+@Data
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="product_name")
+    private String productName;
+    @Column(name="description")
+    private String description;
+    @Column(name="price")
+    private int price;
+    @Column(name="stock_quantity")
+    private int stockQuantity;
+    @Column(name="product_image")
+    private String productImage;
+
+}
